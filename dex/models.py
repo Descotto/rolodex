f35a28901a6ada8bf2134c3eca60fdd2108cc2ab
Descotto <davidescotto@hotmail.com>
2022-12-13 18:45:32 -0800

    create super user and test company

--- dex/models.py
+++ dex/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name}, {self.description}"
+
+class User(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    department = models.CharField(max_length=200)
+    since = models.DateTimeField('date joined')
+
+    def __str__(self):
+        return self.name
