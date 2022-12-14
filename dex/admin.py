7f6063049530bcfea9860973ffcd0971d36bd7eb
Descotto <davidescotto@hotmail.com>
2022-12-13 19:57:54 -0800

    add templates, index

--- dex/admin.py
+++ dex/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib import admin
 
+from .models import Company, User
+
+admin.site.register(Company)
+admin.site.register(User)
 # Register your models here.
--- dex/views.py
+++ dex/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from .models import User, Company
+
 
 
 def index(request):
-    return HttpResponse("Rolodex")
+    companies = list(Company.objects.all())
+    return render(request, "dex/index.html", { 'companies': companies })
+    
+
+   
 
